34b38cd92434dc0c364b7fb466e17f0ce7f5b9c7
Djain1764 <61385076+Djain1764@users.noreply.github.com>
2022-08-24 02:00:30 +0530

    Time: 3089 ms (5.13%), Space: 122.8 MB (5.04%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -1,44 +1,69 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-    ListNode fast = head, slow = head;
-    while (fast != null && fast.next != null) {
-        fast = fast.next.next;
-        slow = slow.next;
-    }
-    if (fast != null) { // odd nodes: let right half smaller
-        slow = slow.next;
-    }
-    slow = reverse(slow);
-    fast = head;
-    
-    while (slow != null) {
-        if (fast.val != slow.val) {
-            return false;
+        ListNode head1 = head ;
+        // ListNode head2 = head ;
+        int n = 1 ;
+        while ( head.next != null ){
+            head = head.next ;
+            n++ ;
         }
-        fast = fast.next;
-        slow = slow.next;
+        ListNode mid1 = head1 ;
+        int x = ( n+1 ) / 2 - 1 ;
+        while ( x > 0 ){
+            x-- ;
+            mid1 = mid1.next ;
+        }
+        ListNode mid2 = mid1.next ;
+        ListNode head2 = Reverse (mid2) ;
+        mid1.next = head2 ;
+        
+        printlist( head1 ) ;
+        printlist( head2 ) ;
+        
+        while ( head2 != null ){
+            if ( head1.val != head2.val ) return false ; 
+            head1 = head1.next ;
+            head2 = head2.next ;
+        }
+        return true ;
+        
+        
     }
-    return true;
-}
-
-public ListNode reverse(ListNode head) {
-    ListNode prev = null;
-    while (head != null) {
-        ListNode next = head.next;
-        head.next = prev;
-        prev = head;
-        head = next;
+    
+    public ListNode Reverse( ListNode mid ){
+        if (mid == null) return null ;
+        if (mid.next == null) return mid ;
+        // ListNode prev = null ;
+        // ListNode curr = mid ;
+        // ListNode far = mid.next ;
+        // while ( far != null ){
+        //     curr.next = prev ;
+        //     prev = curr ;
+        //     curr = far ;
+        //     far = far.next ;
+        // }
+        // return curr ;
+        
+        ListNode prev = null;
+        ListNode current = mid;
+        ListNode next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        mid = prev;
+        return prev;
     }
-    return prev;
-}
+    public void printlist (ListNode headk){
+        while (headk != null ) {
+            if(headk.next != null){
+                System.out.print( headk.val +" -> ") ;
+            }else{
+                System.out.println( headk.val ) ;
+            }
+            headk = headk.next ;
+        }
+    }
 }
