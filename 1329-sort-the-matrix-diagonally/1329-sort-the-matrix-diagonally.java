0b5c89d438121957cede4f8d952f30b6f436f295
Djain1764 <61385076+Djain1764@users.noreply.github.com>
2022-08-29 02:22:36 +0530

    Time: 22 ms (10.99%), Space: 50.2 MB (16.32%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,41 +1,17 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-        
+        HashMap<Integer, PriorityQueue<Integer>> map = new HashMap<>() ;
         for ( int i = 0 ; i < mat.length ; i++ ){
-            countsort (mat, i, 0) ;
-        }
-        for ( int j = 0 ; j < mat[0].length ; j++ ){
-            countsort (mat, 0, j) ;
-        }
-        
-        return mat ;
-        
-        
-        
-    }
-    
-    public void countsort (int[][] mat, int i, int j){
-        int m = mat.length ; 
-        int n = mat[0].length ; 
-        int x = i ;
-        int y = j ;
-        int[] arr = new int[101] ;
-        
-        while ( i < m && j < n ){
-            arr[mat[i][j]]++ ;
-            i++ ;
-            j++ ;
-        }
-        i = x ;
-        j = y ;
-        for ( int k = 0 ; k < arr.length ; k++ ){
-            while (arr[k] > 0){
-                mat[i][j] = k ;
-                arr[k]-- ;
-                i++ ;
-                j++ ;
+            for ( int j = 0 ; j < mat[0].length ; j++ ){
+                map.putIfAbsent(i - j, new PriorityQueue<>() ) ;
+                map.get (i - j).add (mat[i][j]) ;
             }
         }
         
+        for ( int i = 0  ; i < mat.length ; i++ )
+            for ( int j = 0 ; j < mat[0].length ; j++ )
+                mat[i][j] = map.get(i - j).poll() ;
+        
+        return mat ;
     }
 }
