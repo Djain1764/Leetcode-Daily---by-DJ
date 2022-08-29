9021b9bb411b85a171c4a66bd613c15586004bd0
Djain1764 <61385076+Djain1764@users.noreply.github.com>
2022-08-29 14:22:13 +0530

    Time: 3 ms (97.77%), Space: 58.3 MB (11.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,18 +1,19 @@
-public class Solution {
-    char[][] g;
+class Solution {
     public int numIslands(char[][] grid) {
-        int islands = 0;
-        g = grid;
-        for (int i=0; i<g.length; i++)
-            for (int j=0; j<g[i].length; j++)
-                islands += sink(i, j);
-        return islands;
+        int islands = 0 ;
+        for ( int i = 0 ; i < grid.length ; i++ )
+            for ( int j = 0 ; j < grid[0].length ; j++ )
+                islands += sink(grid, i , j) ;
+        return islands ;
     }
-    int sink(int i, int j) {
-        if (i < 0 || i == g.length || j < 0 || j == g[i].length || g[i][j] == '0')
-            return 0;
-        g[i][j] = '0';
-        sink(i+1, j); sink(i-1, j); sink(i, j+1); sink(i, j-1);
-        return 1;
+    public int sink (char[][] grid, int i, int j){
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0')
+            return 0 ;
+        grid[i][j] = '0' ;
+        sink (grid, i + 1, j) ;
+        sink (grid, i - 1, j) ;
+        sink (grid, i , j + 1) ;
+        sink (grid, i , j - 1) ;
+        return 1 ;
     }
 }
