bd0d6d60cc728deaf2db61773f326a5ce356a6f5
Djain1764 <61385076+Djain1764@users.noreply.github.com>
2022-10-07 12:25:18 +0530

    Time: 1138 ms (5.16%), Space: 118 MB (5.41%) - LeetHub

---
+++ 732-my-calendar-iii/732-my-calendar-iii.java
@@ -0,0 +1,22 @@
+class MyCalendarThree {
+
+TreeMap<Integer,Integer> map;
+
+public MyCalendarThree() {
+    map = new TreeMap<>();
+}
+
+public int book(int start, int end) {
+    map.put(start,map.getOrDefault(start,0)+1);
+    map.put(end,map.getOrDefault(end,0)-1);
+    
+    int max = 0;
+    int active = 0;
+    for(int i : map.keySet()){
+        active += map.get(i);
+        max = Math.max(max,active);
+    }
+    
+    return max;
+}
+}
