be87f8f318b55c0dd2195c1237f1d030795eb3ad
Djain1764 <61385076+Djain1764@users.noreply.github.com>
2022-10-18 15:08:43 +0530

    Time: 4 ms (85.69%), Space: 41.8 MB (66.45%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -5,14 +5,15 @@ class Solution {
         }
         String str = countAndSay(n - 1) ;
         int c = 0 ;
-        String output = "" ;
+        // String output = "" ;
+        StringBuilder res = new StringBuilder() ;
         for( int i = 0 ; i < str.length() ; i++ ){
             c++ ;
             if (i == str.length() - 1 || str.charAt(i) != str.charAt(i + 1)){
-                output = output + c + str.charAt(i) ;
+                res.append(c).append(str.charAt(i)) ;
                 c = 0 ;
             }
         }
-        return output ;
+        return res.toString() ;
     }
 }
